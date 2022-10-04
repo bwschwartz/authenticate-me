0c2b35a4b002b92fc7480d38b56ec87da2c4ff9d
Benjamin Schwartz <bs4308@nyu.edu>
2022-10-04 07:37:25 -0400

    Finish setup

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,12 @@ module AuthenticateMe
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    # Add cookie support
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore,
+      key: '_auth_me_session',
+      same_site: :lax,
+      secure: Rails.env.production?
 
     # Configuration for the application, engines, and railties goes here.
     #
